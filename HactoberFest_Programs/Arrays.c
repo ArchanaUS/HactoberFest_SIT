ee978871a603e857b572c91af49d8739ddf6e550
ArchanaUS <57026753+ArchanaUS@users.noreply.github.com>
2019-10-26 10:55:58 +0530

    removed i
    initialization of i
    array elements are inserted

--- HactoberFest_Programs/Arrays.c
+++ HactoberFest_Programs/Arrays.c
@@ -1,9 +1,9 @@
 //Provide indentation and necessary documentation
 
 #include<stdio.h>
-int main2(){
-   int numArr[10]={0};
-   int i=0;
+int main(){
+   int numArr[10]={1,2,3,4,5,6,7,8,9,10};
+   int i;
    for(i=0;i<10;i++){
     scanf("%d",&numArr[i]);
    }
